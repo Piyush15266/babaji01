a4b0406ae27f69fb4d8e26a0334d16b5852a9695
ayushab06 <ab06ayush@gmail.com>
2020-08-01 10:32:50 +0530

    added a comment

---
+++ shell.java
@@ -0,0 +1,25 @@
+public class shell {
+    //this is shell sort.
+    public static void main(String[] args)
+    {
+    int arr[]={20,35,-15,7,55,1,-22};
+    for(int gap=arr.length/3;gap>0;gap/=2)
+    {
+        for(int unsorted=gap;unsorted<arr.length;unsorted++)
+        {
+            int largest=arr[unsorted];
+            int j;
+            for(j=unsorted;j-gap>=0 && largest<arr[j-gap];j-=gap)
+            {
+                arr[j]=arr[j-gap];
+            }
+            arr[j]=largest;           
+        }
+    }
+        for(int i:arr)
+        {
+            System.out.println(i);
+        }
+        
+    } 
+}
